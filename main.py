00a512f4befe041385be53533a7cef54a1f2dae8
AdeebNqo <adeebnqo@gmail.com>
2014-03-08 13:59:16 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+#
+# Zola Mahlaza
+# 8 March 2014
+#
+import smtpd
+import hashlib
+def main():
+	print('hello')
+
+#
+# Security utility
+#
+class Security():
+	#
+	# Method for computing
+	# a hash using a provided algorithm
+	#
+	def hash(File, hashAlgo):
+		return ''
+	#
+	#Method for encrypting data
+	#using a specified algorithm
+	#
+	def encrypt(EncryptAlgo):
+		return ''
+if __name__=='__main__':
+	main()
