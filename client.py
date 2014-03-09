73120b0c523f467e723484880fc3f59ecb8fd0fb
AdeebNqo <adeebnqo@gmail.com>
2014-03-09 18:55:52 +0200

    about to added three way handshake

--- client.py
+++ client.py
@@ -3,20 +3,34 @@
 # 09 March 2014
 #
 import socket
+import pickle
 class client(object):
 	sockt = None
 	host = None
 	port = 0
+	publickeyLocation = None
+	privatekeyLocation = None
 	def __init__(self, host, port):
+		#self.savekeyConfig('./keys','./keys')
 		self.host = host
 		self.port = port
 		self.sockt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		config = pickle.load(open('./data/client_config.pkl','rb'))
+		self.publickeyLocation = config['publickey']
+		self.privatekeyLocation = config['privatekey']
 	def connect(self):
 		self.sockt.connect((self.host, self.port))
+		#initiate three-way handshake
+		print('keys are in {} folder'.format(self.publickeyLocation))
 	def disconnect(self):
 		self.sockt.close()
 	def send(self,data):
 		self.sockt.sendall(data)
+	def savekeyConfig(self, publickey, privatekey):
+		config = {}
+		config['publickey'] = publickey
+		config['privatekey'] = privatekey
+		pickle.dump(config, open('./data/client_config.pkl','w'))
 if __name__=='__main__':
 	client = client('localhost', 7777)
 	client.connect()
