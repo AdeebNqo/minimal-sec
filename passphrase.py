7b53f91fc8e51ee9c34d515cb906afc9684f72d8
AdeebNqo <adeebnqo@gmail.com>
2014-03-26 17:00:11 +0200

    adding random passphrase support

---
+++ passphrase.py
@@ -0,0 +1,45 @@
+#
+# Zola Mahlaza (AdeebNqo)
+# 26 March 2014
+#
+# Passphrase generator, wrapper for python faker https://github.com/joke2k/faker
+#
+# "The best thing about a boolean is even if you are wrong, you are only off by a bit."
+# -Anonymous
+#
+import random
+from faker import Factory
+locale = [
+	'de_DE',
+	'cs_CZ',
+	'dk_DK',
+	'el_GR',
+	'en_CA',
+	'en_GB',
+	'en_US',
+	'es_ES',
+	'es_MX',
+	'fa_IR',
+	'fi_FI',
+	'fr_FR',
+	'it_IT',
+	'ko_KR',
+	'pl_PL',
+	'pt_BR',
+	'ru_RU',
+	'zh_CN'
+	]
+lang = random.randint(0,len(locale)-1)
+fake = Factory.create(lang)
+categories = [
+	'name','company','address'
+	]
+getpassphrase():
+	choice = categories[random.randint(0,3)]
+	if choice=='name'):
+		return fake.name()
+	elif choice=='company'):
+		return fake.company()
+	elif choice=='address'):
+		return fake.address()
+	return None
