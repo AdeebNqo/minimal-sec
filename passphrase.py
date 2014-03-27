5b0b81e5517f10abc27c9e08d1c4ad5c09af9eb3
AdeebNqo <adeebnqo@gmail.com>
2014-03-27 08:40:39 +0200

    random.randint is inclusive of upper bound so values are between 0 and 2

--- passphrase.py
+++ passphrase.py
@@ -10,22 +10,22 @@
 import random
 from faker import Factory
 locale = [
-	'de_DE',
-	'cs_CZ',
-	'dk_DK',
-	'el_GR',
+	#'de_DE',
+	#'cs_CZ',
+	#'dk_DK',
+	#'el_GR',
 	'en_CA',
 	'en_GB',
-	'en_US',
-	'es_ES',
-	'es_MX',
-	'fa_IR',
-	'fi_FI',
-	'fr_FR',
-	'it_IT',
-	'ko_KR',
-	'pl_PL',
-	'pt_BR'
+	'en_US'
+	#'es_ES',
+	#'es_MX',
+	#'fa_IR',
+	#'fi_FI',
+	#'fr_FR',
+	#'it_IT',
+	#'ko_KR',
+	#'pl_PL',
+	#'pt_BR'
 	]
 lang = locale[random.randint(0,len(locale)-1)]
 fake = Factory.create(lang)
@@ -33,7 +33,7 @@ categories = [
 	'name','company','address'
 	]
 def getpassphrase():
-	choice = categories[random.randint(0,3)]
+	choice = categories[random.randint(0,2)]
 	if (choice=='name'):
 		return fake.name()
 	elif (choice=='company'):
